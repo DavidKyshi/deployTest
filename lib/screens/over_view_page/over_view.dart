e1b1db960f76afb91901bc40b61aab80b8a92a04
Tovia <69795021+Tedionwe@users.noreply.github.com>
2023-03-11 12:32:56 +0100

    merged

--- lib/screens/over_view_page/over_view.dart
+++ lib/screens/over_view_page/over_view.dart
@@ -2,7 +2,7 @@
 
 import 'package:kyshi_operations_dashboard/models/express_chart.dart';
 import 'package:kyshi_operations_dashboard/styleguide/colors.dart';
-// import 'package:syncfusion_flutter_charts/charts.dart';
+ import 'package:syncfusion_flutter_charts/charts.dart';
 
 import '../../helper/screen_export.dart';
 import '../../providers/over_view_provider.dart';
@@ -21,11 +21,11 @@ class OverViewScreen extends StatefulWidget {
 }
 
 class _OverViewScreenState extends State<OverViewScreen> {
-  // late List<StatusData> _chartData;
+   late List<StatusData> _chartData;
   @override
   void initState() {
     super.initState();
-    // _chartData = getChartData();
+     _chartData = getChartData();
   }
 
   OverViewProvider get overViewProvider =>
@@ -37,33 +37,33 @@ class _OverViewScreenState extends State<OverViewScreen> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
-              child: Row(
-                children: [
-                  OverViewOffers(),
-                  SizedBox(
-                    width: 40,
-                  ),
-                  OverViewMarketPlace(),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
-              child: Row(
-                children: [
-                  OverViewConnect(),
-                  SizedBox(
-                    width: 40,
-                  ),
-                  OverViewAirtime()
-                ],
-              ),
-            ),
+            // Padding(
+            //   padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
+            //   child: Row(
+            //     children: [
+            //       OverViewOffers(),
+            //       SizedBox(
+            //         width: 40,
+            //       ),
+            //       OverViewMarketPlace(),
+            //     ],
+            //   ),
+            // ),
+            // SizedBox(
+            //   height: 30,
+            // ),
+            // Padding(
+            //   padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
+            //   child: Row(
+            //     children: [
+            //       OverViewConnect(),
+            //       SizedBox(
+            //         width: 40,
+            //       ),
+            //       OverViewAirtime()
+            //     ],
+            //   ),
+            // ),
             SizedBox(
               height: 30,
             ),
@@ -78,61 +78,64 @@ class _OverViewScreenState extends State<OverViewScreen> {
                   OverViewExpressChart()
                 ],
               ),
-            )
-            // Container(
-            //   width: 651.67,
-            //   height: 338.4,
-            //   decoration: BoxDecoration(
-            //     borderRadius: BorderRadius.circular(10),
-            //     border: Border.all(color: const Color(0xffE8E8E8))
-            //   ),
-            //   child: Column(
-            //     children: [
-            //       const Text('Express Transfers',
-            //       style: TextStyle(
-            //         color: Color(0xff0D2C65),
-            //         fontWeight: FontWeight.w400,
-            //         fontSize: 18
-            //       ),
-            //       ),
-            //       Row(
-            //         children: [
-            //           SfCircularChart(series: <CircularSeries>[
-            //             DoughnutSeries<StatusData, String>(
-            //               dataSource: _chartData,
-            //               xValueMapper: (StatusData data, _)=> data.status,
-            //                yValueMapper: (StatusData data, _)=> data.amount,
-            //                dataLabelSettings: DataLabelSettings(isVisible: true) ,
-            //               //  explode: true,
-            //               //           explodeIndex: 1
+            ),
+            Container(
+              width: 651.67,
+              height: 338.4,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: const Color(0xffE8E8E8))
+              ),
+              child: Column(
+                children: [
+                  const Text('Express Transfers',
+                  style: TextStyle(
+                    color: Color(0xff0D2C65),
+                    fontWeight: FontWeight.w400,
+                    fontSize: 18
+                  ),
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        height: 400,
+                        child: SfCircularChart(series: <CircularSeries>[
+                          DoughnutSeries<StatusData, String>(
+                            dataSource: _chartData,
+                            xValueMapper: (StatusData data, _)=> data.status,
+                             yValueMapper: (StatusData data, _)=> data.amount,
+                             dataLabelSettings: DataLabelSettings(isVisible: true) ,
+                            //  explode: true,
+                            //           explodeIndex: 1
 
-            //                )
-            //           ],)
-            //         ],
-            //       )
-            //     ],
-            //   ),
-            // ),
+                             )
+                        ],),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 
-  // List<StatusData> getChartData(){
-  //    final expressChart = overViewProvider.expressChart;
-  //   final List<StatusData> chartData = [
-  //    StatusData("Successful",  expressChart.first.successfullCountExpressTransfer),
-  //    StatusData("Pending",  expressChart.first.pendingCountExpressTransfer),
-  //    StatusData("Failed",  expressChart.first.failedCountExpressTransfer)
-  //   ];
-  //   return chartData;
-  // }
+  List<StatusData> getChartData(){
+     final expressChart = overViewProvider.expressChart;
+    final List<StatusData> chartData = [
+     StatusData("Successful",  20),
+     StatusData("Pending",  50),
+     StatusData("Failed",  30)
+    ];
+    return chartData;
+  }
 }
-// class StatusData{
-//   StatusData( this.status, this.amount,);
+class StatusData{
+  StatusData( this.status, this.amount,);
 
-//   final String status;
-//    final int? amount;
-//   //  final Color color;
-// }
+  final String status;
+   final int? amount;
+  //  final Color color;
+}
